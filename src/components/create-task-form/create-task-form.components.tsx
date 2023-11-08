127858c82f4559d42c9cd871aa538e0ef9b31875
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-11-08 04:01:22 +0000

    style: styling the Box and Typography components from mui

--- src/components/create-task-form/create-task-form.components.tsx
+++ src/components/create-task-form/create-task-form.components.tsx
@@ -4,8 +4,20 @@ import {Box, Typography} from '@mui/material'
 
 export const CreateTaskForm:FC = ():ReactElement =>{
     return(
-            <Box>
-                <Typography>Create a Task</Typography>
+            <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            >
+                <Typography
+                sx={{
+                    width: '96px',
+                    height: '96px',
+                    backgroundColor: 'primary.main',
+                    marginBottom: '16px',
+                  }}
+                >Create a Task</Typography>
             </Box>
           )
 }
