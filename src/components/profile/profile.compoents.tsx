562a39017f202b03df9609a5d5089d160e6517c9
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-11-08 02:40:58 +0000

    feat: added the name and welcome section

--- src/components/profile/profile.compoents.tsx
+++ src/components/profile/profile.compoents.tsx
@@ -5,13 +5,31 @@ export const Profile:FC = ():ReactElement => {
     return (
         <Box
         display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
         >
             <Avatar 
-            
-            >D</Avatar>
-        </Box>
+             sx={{
+                width: '96px',
+                height: '96px',
+                backgroundColor: 'primary.main',
+                marginBottom: '16px',
+              }}
+            >
+              <Typography variant="h4" color="text.primary">
+                DG
+            </Typography>
+            </Avatar>
+
+            {/* name */}
+            <Typography variant="h6" color="text.primary">
+              Desmond Goldsmith
+            </Typography>
+            {/* welcome section */}
+            <Typography variant="body1" color="text.primary">
+            Welcome, Desmond. This is your personal tasks manager!
+            </Typography>
+            </Box>
     )
 } 
