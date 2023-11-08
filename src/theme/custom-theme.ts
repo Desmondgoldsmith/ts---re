dfe8ae6363b09ad26ebd66aa67507b196fef9b80
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-11-08 01:27:19 +0000

    feat: building the dashboard page

--- src/theme/custom-theme.ts
+++ src/theme/custom-theme.ts
@@ -1,19 +1,16 @@
 import {createTheme,ThemeOptions} from '@mui/material'
 
 export const customTheme:ThemeOptions = createTheme({
-    palette:{
+    palette: {
         mode: 'dark',
-        primary:{
-            main: '#1976d2',
-            light: '#42a5f5',
-            dark: '#1565c0',
-            contrastText: '#fff'  
+        primary: {
+          light: 'rgba(168,85,247,.80)',
+          main: 'rgba(168,85,247,.65)',
+          dark: 'rgba(168,85,247,.28)',
         },
-        background : {
-            paper: '#fff',
-            default: '#fff'  
+        background: {
+          paper: '#151515',
+          default: 'rgba(0,0,0,.96)',
         },
-
-
-    }
+      },
 }) 
