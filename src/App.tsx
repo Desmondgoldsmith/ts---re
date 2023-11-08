e86a66cac571a0693efed8d5bcaa4f08a9c9d2b4
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-11-08 01:38:46 +0000

    feat: building the dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React, {FC,ReactElement} from 'react';
 //FC =functional components {its an interface}
 import {ThemeProvider, CssBaseline} from '@mui/material'
 import {customTheme} from './theme/custom-theme'
+import {Dashboard} from './pages/dashboard/dashboard-pages'
+
 const App:FC = ():ReactElement => {
   return (
     <ThemeProvider theme = {customTheme}>
       <CssBaseline/>
-      <div className="App">
-     <h1>Hello World</h1>   
-    </div>
+      <Dashboard/>
     </ThemeProvider>
   );
 }
