4ea0fa5ac8e4cecfd5050230d636081301ca5e05
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-11-08 03:55:37 +0000

    feat: scafolding the CreateTaskForm component

---
+++ src/components/create-task-form/create-task-form.components.tsx
@@ -0,0 +1,11 @@
+import React, {FC, ReactElement} from 'react'
+import {Box, Typography} from '@mui/material'
+
+
+export const CreateTaskForm:FC = ():ReactElement =>{
+    return(
+            <Box>
+                <Typography>Create a Task</Typography>
+            </Box>
+          )
+}
