b810c041816656106dce094b6701f5da778c8cf2
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-11-15 23:46:48 +0000

    chore: making use of  propTypes

--- src/components/create-task-form/_task_descriptionField.tsx
+++ src/components/create-task-form/_task_descriptionField.tsx
@@ -1,7 +1,7 @@
 import React, {FC,ReactElement} from 'react'
 import {TextField} from '@mui/material'
 import {ITextfield} from './interfaces/ITextfield'
-
+import propTypes from 'prop-types'
 
 export const DescriptionField:FC<ITextfield> = ({onChange = (e) => {console.log(e)}, disabled = false}):ReactElement => {
     return(
@@ -19,4 +19,9 @@ export const DescriptionField:FC<ITextfield> = ({onChange = (e) => {console.log(
         disabled = {disabled}
         />
     )
+}
+
+DescriptionField.propTypes = {
+    onChange: PropTypes.func,
+    disabled: PropTypes.bool,
 }
